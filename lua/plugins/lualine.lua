f687d1aac3e99af5be2ee53e9cc1d7cc531616b7
Shahe <shahe@Shahes-MacBook-Pro-2.local>
2025-10-31 22:58:02 +0000

    remove clock

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -118,13 +118,7 @@ return {
 						{ "progress", separator = " ", padding = { left = 1, right = 0 } },
 						{ "location", padding = { left = 0, right = 1 } },
 					},
-					lualine_z = {
-						{
-							function()
-								return " " .. os.date("%R")
-							end,
-						},
-					},
+					lualine_z = {},
 				},
 				inactive_sections = {
 					lualine_a = {},

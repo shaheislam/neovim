3479aafe3f3ec357a3fa67ec838c7954cb4746bc
Shahe <shahe@Shahes-MacBook-Pro-2.local>
2025-10-31 23:02:49 +0000

    install neoscroll

---
+++ lua/plugins/neoscroll.lua
@@ -0,0 +1,16 @@
+-- Neoscroll - Smooth scrolling animations
+-- Provides aesthetic smooth scrolling for better visual experience
+
+return {
+  {
+    "karb94/neoscroll.nvim",
+    event = "VeryLazy",
+    opts = {
+      mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "zt", "zz", "zb" },
+      hide_cursor = true,
+      stop_eof = true,
+      respect_scrolloff = false,
+      cursor_scrolls_alone = true,
+    },
+  },
+}

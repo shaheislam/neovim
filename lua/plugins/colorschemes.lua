2d31600a8b92aa6125ac007d331958730bab847b
Shahe <shahe@Shahes-MacBook-Pro-2.local>
2025-11-01 00:26:17 +0000

    Revert "trim colorschemes"
    This reverts commit 96be3aa1299bf4971a681c960c16fefb9622e472.

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -47,17 +47,41 @@ return {
     },
   },
 
-  -- Rose Pine Moon
+  -- Kanagawa
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 997,
+    opts = {
+      transparent = true,
+      terminal_colors = true,
+    },
+  },
+
+  -- Rose Pine
   {
     "rose-pine/neovim",
     name = "rose-pine",
     lazy = false,
-    priority = 997,
+    priority = 996,
     opts = {
-      variant = "moon",
-      dark_variant = "moon",
+      variant = "main",
+      dark_variant = "main",
       styles = {
         transparency = true,
+      },
+    },
+  },
+
+  -- Nightfox
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 995,
+    opts = {
+      options = {
+        transparent = true,
+        terminal_colors = true,
       },
     },
   },
@@ -66,7 +90,7 @@ return {
   {
     "projekt0n/github-nvim-theme",
     lazy = false,
-    priority = 996,
+    priority = 992,
     config = function()
       require("github-theme").setup({
         options = {
@@ -75,5 +99,52 @@ return {
         },
       })
     end,
+  },
+
+  -- Gruvbox Material
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 994,
+    config = function()
+      vim.g.gruvbox_material_background = "medium"
+      vim.g.gruvbox_material_transparent_background = 1
+    end,
+  },
+
+  -- Everforest
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 993,
+    config = function()
+      vim.g.everforest_background = "medium"
+      vim.g.everforest_transparent_background = 1
+      vim.g.everforest_better_performance = 1
+    end,
+  },
+
+  -- Nord
+  {
+    "shaunsingh/nord.nvim",
+    lazy = false,
+    priority = 990,
+    config = function()
+      vim.g.nord_contrast = true
+      vim.g.nord_borders = false
+      vim.g.nord_disable_background = true
+    end,
+  },
+
+  -- Cyberdream
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 991,
+    opts = {
+      transparent = true,
+      hide_fillchars = true,
+      borderless_telescope = true,
+    },
   },
 }
